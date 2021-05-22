918250aa70a600ded4c8056789cda2e3d283e62c
fliiiix <hi@l33t.name>
2021-05-22 07:55:30 +0200

    Use black to format code

--- src/wifibackup/wifi.py
+++ src/wifibackup/wifi.py
@@ -13,7 +13,7 @@ def cli():
 
 
 @cli.command("backup", help="Backup all wifi connections from NetworkManager.")
-@click.argument('filename', required=False)
+@click.argument("filename", required=False)
 def backup(filename):
     if not filename:
         filename = "wifi-connections.json"
@@ -24,8 +24,10 @@ def backup(filename):
     print(f"[INFO] Wifi connections backuped to: {filename}")
 
 
-@cli.command("import", help="Import wifi connections from a file to the NetworkManager.")
-@click.argument('filename', required=False)
+@cli.command(
+    "import", help="Import wifi connections from a file to the NetworkManager."
+)
+@click.argument("filename", required=False)
 def _import(filename):
     if not filename:
         filename = "wifi-connections.json"
@@ -52,58 +54,67 @@ def add_connection(connection):
 
 
 def create_wifi_connection_dict(connection):
-    name = connection['name']
-    ssid = connection['ssid'].encode("utf-8")
-    secret_type = connection['secret-type']
+    name = connection["name"]
+    ssid = connection["ssid"].encode("utf-8")
+    secret_type = connection["secret-type"]
 
-    connection_type = dbus.Dictionary({
-        'type': '802-11-wireless',
-        'uuid': str(uuid.uuid4()),
-        'id': name})
+    connection_type = dbus.Dictionary(
+        {"type": "802-11-wireless", "uuid": str(uuid.uuid4()), "id": name}
+    )
 
-    wifi = dbus.Dictionary({
-        'ssid': dbus.ByteArray(ssid),
-        'mode': 'infrastructure',
-    })
+    wifi = dbus.Dictionary(
+        {
+            "ssid": dbus.ByteArray(ssid),
+            "mode": "infrastructure",
+        }
+    )
 
-    wsecurity = dbus.Dictionary({'key-mgmt': 'None'})
+    wsecurity = dbus.Dictionary({"key-mgmt": "None"})
     if secret_type == "wpa-psk":
-        psk = connection['secrets']['psk']
+        psk = connection["secrets"]["psk"]
 
-        wsecurity = dbus.Dictionary({
-            'key-mgmt': 'wpa-psk',
-            'auth-alg': 'open',
-            'psk': psk,
-        })
+        wsecurity = dbus.Dictionary(
+            {
+                "key-mgmt": "wpa-psk",
+                "auth-alg": "open",
+                "psk": psk,
+            }
+        )
     if secret_type == "wpa-eap":
-        secrets = connection['secrets']
+        secrets = connection["secrets"]
 
-        wsecurity = dbus.Dictionary({
-            'key-mgmt': 'wpa-eap',
-            'auth-alg': 'open',
-        })
+        wsecurity = dbus.Dictionary(
+            {
+                "key-mgmt": "wpa-eap",
+                "auth-alg": "open",
+            }
+        )
 
-        wsecurity_enterprise = dbus.Dictionary({
-            'eap': [secrets['eap']],
-            'identity': secrets['identity'],
-            'phase2-auth': secrets['phase2-auth'],
-            'password': secrets['password']
-        })
+        wsecurity_enterprise = dbus.Dictionary(
+            {
+                "eap": [secrets["eap"]],
+                "identity": secrets["identity"],
+                "phase2-auth": secrets["phase2-auth"],
+                "password": secrets["password"],
+            }
+        )
 
-        if secrets['anonymous-identity']:
-            wsecurity_enterprise['anonymous-identity'] = secrets['anonymous-identity']
+        if secrets["anonymous-identity"]:
+            wsecurity_enterprise["anonymous-identity"] = secrets["anonymous-identity"]
 
-    ip_settings = dbus.Dictionary({'method': 'auto'})
-    con = dbus.Dictionary({
-        'connection': connection_type,
-        '802-11-wireless': wifi,
-        '802-11-wireless-security': wsecurity,
-        'ipv4': ip_settings,
-        'ipv6': ip_settings
-    })
+    ip_settings = dbus.Dictionary({"method": "auto"})
+    con = dbus.Dictionary(
+        {
+            "connection": connection_type,
+            "802-11-wireless": wifi,
+            "802-11-wireless-security": wsecurity,
+            "ipv4": ip_settings,
+            "ipv6": ip_settings,
+        }
+    )
 
     if secret_type == "wpa-eap":
-        con['802-1x'] = wsecurity_enterprise
+        con["802-1x"] = wsecurity_enterprise
 
     return con
 
@@ -133,43 +144,45 @@ def list_connections():
 
     for path in connection_paths:
         con_proxy = bus.get_object(service_name, path)
-        settings_connection = dbus.Interface(con_proxy, "org.freedesktop.NetworkManager.Settings.Connection")
+        settings_connection = dbus.Interface(
+            con_proxy, "org.freedesktop.NetworkManager.Settings.Connection"
+        )
         config = settings_connection.GetSettings()
 
-        merge_secrets(settings_connection, config, '802-11-wireless')
-        merge_secrets(settings_connection, config, '802-11-wireless-security')
-        merge_secrets(settings_connection, config, '802-1x')
+        merge_secrets(settings_connection, config, "802-11-wireless")
+        merge_secrets(settings_connection, config, "802-11-wireless-security")
+        merge_secrets(settings_connection, config, "802-1x")
 
-        if (config['connection']['type'] == "802-11-wireless"):
-            ssid = ''.join([chr(byte) for byte in config['802-11-wireless']['ssid']])
+        if config["connection"]["type"] == "802-11-wireless":
+            ssid = "".join([chr(byte) for byte in config["802-11-wireless"]["ssid"]])
 
             sec_type = ""
             sec = {}
-            if ('802-11-wireless-security' in config):
-                sec_type = config['802-11-wireless-security']['key-mgmt']
-                if (sec_type == "wpa-psk"):
-                    if 'psk' in config['802-11-wireless-security']:
-                        sec = { 'psk': f"{config['802-11-wireless-security']['psk']}" }
+            if "802-11-wireless-security" in config:
+                sec_type = config["802-11-wireless-security"]["key-mgmt"]
+                if sec_type == "wpa-psk":
+                    if "psk" in config["802-11-wireless-security"]:
+                        sec = {"psk": f"{config['802-11-wireless-security']['psk']}"}
                     else:
                         print(f"[WARN]: Connection {ssid} exported without password")
-                if (sec_type == "wpa-eap"):
+                if sec_type == "wpa-eap":
                     ano_identity = ""
-                    if('anonymous-identity' in config['802-1x']):
+                    if "anonymous-identity" in config["802-1x"]:
                         ano_identity = f"{config['802-1x']['anonymous-identity']}"
 
                     sec = {
-                      'eap': f"{config['802-1x']['eap'][0]}",
-                      'identity': f"{config['802-1x']['identity']}",
-                      'anonymous-identity': ano_identity,
-                      'phase2-auth': f"{config['802-1x']['phase2-auth']}",
-                      'password': f"{config['802-1x']['password']}"
+                        "eap": f"{config['802-1x']['eap'][0]}",
+                        "identity": f"{config['802-1x']['identity']}",
+                        "anonymous-identity": ano_identity,
+                        "phase2-auth": f"{config['802-1x']['phase2-auth']}",
+                        "password": f"{config['802-1x']['password']}",
                     }
 
             con = {
-              'name': f"{config['connection']['id']}",
-              'ssid': ssid,
-              'secret-type': f"{sec_type}",
-              'secrets': sec
+                "name": f"{config['connection']['id']}",
+                "ssid": ssid,
+                "secret-type": f"{sec_type}",
+                "secrets": sec,
             }
             connections.append(con)
 
@@ -182,5 +195,5 @@ def write_to_file(connections, filename):
             f.write(f"{json.dumps(con)}\n")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli()
